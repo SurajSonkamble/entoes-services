81408a9907d1e021b10e01da3f769ae7922f8183
suraj <surajsonkamble202@gmail.com>
2025-06-09 15:20:47 +0530

    enotes services

---
+++ src/main/java/com/becoder/EnotesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesServicesApplication.class, args);
+	}
+
+}
