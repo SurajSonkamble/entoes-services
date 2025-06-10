e7f1c1005d5cfe738c2bfc0eaf84421d92fa1f32
suraj <surajsonkamble202@gmail.com>
2025-06-10 14:55:47 +0530

    dto mapper code

--- src/main/java/com/becoder/service/CategoryService.java
+++ src/main/java/com/becoder/service/CategoryService.java
@@ -2,14 +2,18 @@ package com.becoder.service;
 
 import java.util.List;
 
+import com.becoder.dto.CategoryDto;
+import com.becoder.dto.CategoryResponse;
 import com.becoder.entities.Category;
 
 public interface CategoryService {
 	
 	
 	
-	public Boolean saveCategory(Category category);
+	public Boolean saveCategory(CategoryDto categoryDto);
 	
 	List<Category> getAllCategory();
+
+	 List<CategoryResponse> getActiveCategorry();
 
 }
